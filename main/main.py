3f94a017ba76327fc338daae76ef9d3065fdac3e
dmitrilay <dmitrilay@gmail.com>
2022-10-04 21:04:27 +0600

    fix

--- main/main.py
+++ main/main.py
@@ -3,9 +3,13 @@ from data_requests import sending_get_data, sending_spec
 from html_processing import startHtmlProcessing
 from files_and_folders import WorkFolderFiles
 
-data = sending_get_data()
-startParserSpecMVM(data)
-data = startHtmlProcessing()
-sending_spec(data)
 
-WorkFolderFiles.file_cleaner()
+def auto():
+    data = sending_get_data()
+    startParserSpecMVM(data)
+    data = startHtmlProcessing()
+    sending_spec(data)
+    WorkFolderFiles.file_cleaner()
+
+
+auto()
