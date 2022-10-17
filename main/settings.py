dd6fe2681089a30eccfe31636920abbf24edb9ce
Dmitriy Nozhkin <dmitrilay@gmail.com>
2022-10-17 16:49:09 +0600

    Рефакторинг

--- main/settings.py
+++ main/settings.py
@@ -1,4 +1,4 @@
-DEBUG = True
+DEBUG = False
 
 
 """Настройки браузера"""
@@ -20,25 +20,10 @@ FOLDERS = [
 ]
 
 
-try:
-    if DEBUG:
-        from settings_local import *
-    else:
-        from settings_dev import *
-except:
-    print('settings_dev', 'settings_local', "файлы не найдены")
-
-    """Авторизация"""
-    url = 'https://online.smartlombard.ru/login/'
-    username = ""
-    password = ""
-
-    """Авторизация на ch-shop.ru"""
-    token = "111111111"
-
-    """Данные для сайта"""
-    PATH_NAME = 'smartlombard/v2/v2/v3'
-    HOST = 'http://127.0.0.1:8000/'
+if DEBUG:
+    from settings_local import *
+else:
+    from settings_dev import *
 
 
 """Категории"""
