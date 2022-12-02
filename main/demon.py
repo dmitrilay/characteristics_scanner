f6a7237719db3f1438b5f7fdb238cbe127b86324
Dmitriy Nozhkin <dmitrilay@gmail.com>
2022-12-03 02:06:18 +0600

    fix demon

--- main/demon.py
+++ main/demon.py
@@ -3,10 +3,24 @@ import threading
 import time
 from main import auto
 
+PAUSE = 120
+
+
+def timer():
+    print(f"Пауза {PAUSE} секунд")
+    time.sleep(PAUSE)
+    print("Запуск")
+    for t in range(1, 10):
+        print(f' -- {t} -- ')
+        time.sleep(1)
+
 
 while True:
-    print('Поток запущен')
-    _th = threading.Thread(target=auto, args=(), name='thr-1')
-    _th.start()
-    _th.join()
-    time.sleep(120)
+    try:
+        print('Поток запущен')
+        _th = threading.Thread(target=auto, args=(), name='thr-1')
+        _th.start()
+        _th.join()
+        timer()
+    except:
+        print('Ошибка')
